4c3678c1f9648c3c12736383ea22c600d6b74166
Ruslan <66295121+BlueRexPY@users.noreply.github.com>
2022-10-06 13:33:02 +0200

    Add files via upload

---
+++ laboratory/lab1-dnu/task2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+double calculate(double a, double b, double c) {
+	if (a<=b)
+	{
+		return (a+b)*c;
+	} 
+	return 0;
+}
+
+
+int main()
+{
+	double a,b,c = 0;
+
+	cout << "Input a: ";
+	cin >> a;
+	cout << "Input b: ";
+	cin >> b;
+	cout << "Input c: ";
+	cin >> c;
+
+	double y = calculate(a,b,c);
+	cout << y << endl;
+	return 0;
+}
---
+++ laboratory/lab1-dnu/task3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double n = 0;
+	double Sum = n;
+	cout << "Input km: ";
+	cin >> n;
+
+	for (int i = 2;i < 8; i++) {
+		n *= 1.1;
+		Sum += n;
+		cout << "Day " << i << " - " << n << "km" << endl;
+		
+	}
+	cout << "Sum " << Sum << "km" << endl;
+	return 0;
+}
